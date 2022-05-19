d6b3ea51681a4f5442b0298bad6438e4cb7bed53
merzkii <chlaidze10@gmail.com>
2022-05-19 17:07:21 +0400

    week10

---
+++ week10/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week10
+{
+
+    public abstract class FileWorker
+    {
+        public int MaxFileSize { get; set; }
+        public abstract string WidthOfFile { get; set; }
+        public abstract void read();
+        public abstract void write();
+        public abstract void edit();
+        public abstract void delete();
+
+
+
+    }
+
+
+    public class ReadFileworker : FileWorker
+    {
+        public override string WidthOfFile { get; set; }
+
+
+
+        public override void read()
+        {
+            Console.WriteLine($"i can read from {WidthOfFile} file with max storage {MaxFileSize}");
+        }
+        public override void write()
+        {
+            Console.WriteLine($"i can write from {WidthOfFile} file with max storage {MaxFileSize}");
+        }
+        public override void edit()
+        {
+            Console.WriteLine($"i can edit from {WidthOfFile} file with max storage {MaxFileSize}");
+        }
+        public override void delete()
+        {
+            Console.WriteLine($"i can delete from {WidthOfFile} file with max storage {MaxFileSize}");
+        }
+
+    }
+
+}
+
+
+
+
+    
+
+    
